681136a66da2df1de40436977e0728ea6a2762f9
10trips <ethan.freckleton@gmail.com>
2023-11-15 10:46:32 -0600

    Add code

---
+++ main.py
@@ -0,0 +1,22 @@
+class trigger:
+    def __init__(self, x, y, fun, draw, color):
+        
+        self.x = x
+        self.y = y
+        self.myfun = fun
+        self.draw = draw
+        self.color = color
+    def printsome(self):
+        print(self.x,self.y)
+        self.myfun()
+        if draw == True:
+            #draw with color
+
+def test():
+    print("world")
+
+print("test1")
+
+
+mytrigger = trigger(100,100, test, 0,0)
+mytrigger.printsome()
